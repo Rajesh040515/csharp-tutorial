b3900f1770766ff9673332dc4aae6a796bebac78
Rajesh <rajesh.facile@outlook.com>
2021-03-13 00:46:17 -0500

    array function of c#

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = new[] { 1, 2, 3, 4, 5 };
+
+            //array length
+            Console.WriteLine("length"+ number.Length);
+            
+            //index number find
+            var index = Array.IndexOf(number, 2);
+            Console.WriteLine(index);
+
+            //clear
+            Array.Clear(number, 1, 2);
+            Console.WriteLine("clear()");
+            foreach(var n in number)
+                Console.WriteLine(n);
+            //copy
+            int[] another = new int[3];
+            Array.Copy(number, another, 3);
+            Console.WriteLine("copy");
+            foreach (var i in another) Console.WriteLine(i);
+
+            Array.Sort(another);
+            Console.WriteLine("sort");
+            foreach(var k in another)
+                Console.WriteLine(k);
+
+
+            
+
+        
+        }
+    }
+}
