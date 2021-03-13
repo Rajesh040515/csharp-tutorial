9015994050953db4960b0086f5fee43682bdfc8e
Rajesh <rajesh.facile@outlook.com>
2021-03-13 06:06:37 -0500

    date time

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,16 +8,30 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
 
-            var random = new Random();
-            //    for (var i = 0;i<= 10; i++)
-            //    Console.WriteLine(random.Next(1,10));
-            var buffer = new char[10];
-            for(var i = 0; i < 8; i++)
-            {
-                buffer[i]=(char)("a" + random.Next(1,10));
-            var password = new string(buffer);
-            Console.WriteLine(password);
-            }
+            var datetime = new DateTime();
+            var now = DateTime.Now;
+            var today = DateTime.Today;
+            //Console.WriteLine(now.Hour);
+            //Console.WriteLine(now.Minute);
+            //Console.WriteLine(today.Hour);
+            //Console.WriteLine(today.Day);
+            //Console.WriteLine(today.Month);
+
+            var tommorow = now.AddDays(1);
+            Console.WriteLine(tommorow);
+
+            var yesterday = now.AddDays(-1);
+            Console.WriteLine(yesterday);
+
+            Console.WriteLine(now.ToLongDateString());
+            Console.WriteLine(now.ToShortTimeString());
+            Console.WriteLine(now.ToLongTimeString());
+            Console.WriteLine(now.ToShortDateString());
+            Console.WriteLine(now.ToString());
+            
+
+           
+        }
 
             }
           
@@ -27,6 +41,6 @@ namespace ConsoleApp1
 
 
 
-        }
+        
     
 
