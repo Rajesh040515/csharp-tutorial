db235a50b02142288a026a2321238a4e72330068
Rajesh <rajesh.facile@outlook.com>
2021-03-14 00:18:18 -0500

    summarize string

--- C Sharp Tutorials/C Sharp Tutorials/Program.cs
+++ C Sharp Tutorials/C Sharp Tutorials/Program.cs
@@ -6,32 +6,16 @@ namespace C_Sharp_Tutorials
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var sentence = "this is a sentence this is a sentence this is a sentence";
+          
+            var summary =stringutility.summarizetext(sentence, 25);
+            Console.WriteLine(summary);
 
-            var firstname = "rajesh patel";
-            Console.WriteLine(firstname.Trim());
-            Console.WriteLine(firstname.Trim().ToUpper());
+            }
 
-            var index = firstname.IndexOf("rajesh");
-            var fname = firstname.Substring(0, index);
-            var lname = firstname.Substring(index + 1);
-            Console.WriteLine("fname" + fname);
-            Console.WriteLine("lname " + lname);
-
-            var names = firstname.Split(' ');
-            Console.WriteLine(names[0]);
-            Console.WriteLine(names[1]);
-
-            var rename = firstname.Replace();
-            Console.WriteLine("Replace name :" + rename);
-
-            var str = "21";
-            var age = Convert.ToByte(str);
-            Console.WriteLine("Age is" + age);
-
-            double price = 10.234;
-            Console.WriteLine(price.ToString("C"));
-
+          
         }
+
+
     }
 }
---
+++ C Sharp Tutorials/C Sharp Tutorials/stringutility.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Tutorials
+{
+    public class stringutility
+    {
+
+        public static string summarizetext(string text, int maxlength = 0)
+        {
+
+
+            if (text.Length < maxlength)
+                return text;
+            else
+            {
+
+                var words = text.Split(' ');
+                var totalchar = 0;
+                var summarywords = new List<string>();
+                foreach (var word in words)
+                {
+                    summarywords.Add(word);
+
+                    totalchar += word.Length + 1;
+                    if (totalchar > text)
+                        break;
+
+                }
+                var summary = string.Join("", summarywords + "");
+                Console.WriteLine(summary);
+
+
+            }
+        }
+    }
+}
+
+
